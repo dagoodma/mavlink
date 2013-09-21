6b0ae56b56c5a0270bf47ac789171002d1889fc0
David Goodman <dagoodma@gmail.com>
2013-09-20 17:21:34 -0700

    Added new log conversion tools.

---
+++ pymavlink/tools/mavlogdump_samplehold.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+'''
+example program that dumps a Mavlink log file. The log file is
+assumed to be in the format that qgroundcontrol uses, which consists
+of a series of MAVLink packets, each with a 64 bit timestamp
+header. The timestamp is in microseconds since 1970 (unix epoch)
+'''
+
+import sys, time, os, struct, json
+from collections import OrderedDict
+
+# allow import from the parent directory, where mavlink.py is
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+
+from optparse import OptionParser
+parser = OptionParser("mavlogdump.py [options] <LOGFILE>")
+
+parser.add_option("--debug",dest="debug",action='store_true',help="Debug messages")
+parser.add_option("--no-timestamps",dest="notimestamps", action='store_true', help="Log doesn't have timestamps")
+parser.add_option("--planner",dest="planner", action='store_true', help="use planner file format")
+parser.add_option("--robust",dest="robust", action='store_true', help="Enable robust parsing (skip over bad data)")
+parser.add_option("-f", "--follow",dest="follow", action='store_true', help="keep waiting for more data at end of file")
+parser.add_option("--condition",dest="condition", default=None, help="select packets by condition")
+parser.add_option("-q", "--quiet", dest="quiet", action='store_true', help="don't display packets")
+parser.add_option("-o", "--output", default=None, help="output matching packets to give file")
+parser.add_option("--format", dest="format", default=None, help="Change the output format between 'standard', 'json', and 'csv'. For the CSV output, you must supply types that you want.")
+parser.add_option("--csv_sep", dest="csv_sep", default=",", help="Select the delimiter between columns for the ouput CSV file. Use 'tab' to specify tabs. Only applies when --format=csv")
+parser.add_option("--types",  default=None, help="types of messages (comma separated)")
+parser.add_option("--dialect",  default="ardupilotmega", help="MAVLink dialect")
+(opts, args) = parser.parse_args()
+
+import inspect
+import pprint
+import mavutil
+from mavutil import get_dialect_module
+#import mavlink
+
+if len(args) < 1:
+    print("Usage: mavlogdump.py [options] <LOGFILE>")
+    sys.exit(1)
+
+filename = args[0]
+mlog = mavutil.mavlink_connection(filename, planner_format=opts.planner,
+                                  notimestamps=opts.notimestamps,
+                                  robust_parsing=opts.robust,dialect=opts.dialect)
+
+output = None
+if opts.output:
+    output = mavutil.mavlogfile(opts.output, write=True)
+
+types = opts.types
+if types is not None:
+    types = types.split(',')
+
+if opts.debug:
+    print("Building " + opts.format + " file with types: " + str(types))
+
+opts.csv_sep = opts.csv_sep.replace("tab","\t")
+
+# Write out a header row as we're outputting in CSV format.
+fields = []
+fields_header = ""
+offsets = {}
+if opts.format == 'csv':
+    try:
+        currentOffset = 1 # Store how many fields in we are for each message.
+        for type in types:
+            try:
+                typeClass = "MAVLink_{0}_message".format(type.lower())
+                fields += [type + '.' + x for x in inspect.getargspec(getattr(get_dialect_module(), typeClass).__init__).args[1:]]
+                offsets[type] = currentOffset
+                currentOffset += len(fields)
+            except IndexError, e:
+                quit()
+    except TypeError, e:
+        print("You must specify a list of message types if outputting CSV format via the --types argument.")
+        exit()
+
+# Build field header and structs for message fields to sample and hold 
+window = OrderedDict()
+for field in fields:
+    if fields_header:
+        fields_header += opts.csv_sep + " "
+
+    fields_header += field.replace('.', '_')
+    window[field] = 0
+
+fields_header = fields_header.rstrip(opts.csv_sep)
+print(fields_header.upper())
+
+if opts.debug:
+    print("Initialized sample window fields: " + str(window))
+
+while True:
+    m = mlog.recv_match(condition=opts.condition, blocking=opts.follow)
+    if m is None:
+        break
+
+    if types is not None and m.get_type() not in types and m.get_type() != 'BAD_DATA':
+        continue
+    last_timestamp = 0
+    
+    if m.get_type() == 'BAD_DATA' and m.reason == "Bad prefix":
+        continue
+    
+    if output:
+        timestamp = getattr(m, '_timestamp', None)
+        if not timestamp:
+            timestamp = last_timestamp
+        last_timestamp = timestamp
+        output.write(struct.pack('>Q', timestamp*1.0e6))
+        output.write(m.get_msgbuf())
+    if opts.quiet:
+        continue
+
+    # If JSON was ordered, serve it up. Split it nicely into metadata and data.
+    if opts.format == 'json':
+        data = m.to_dict()
+        del data['mavpackettype']
+        outMsg = {"meta": {"msgId": m.get_msgId(), "type": m.get_type(), "timestamp": m._timestamp}, "data": json.dumps(data)}
+        print(outMsg)
+    elif opts.format == 'csv':
+        data = m.to_dict()
+        type = m.get_type()
+        
+        # Iterate over all fields of data and set the window
+        for field,value in data.items():
+            key = type + "." + field
+            if (key in window):
+                window[key] = value
+
+        # Build the line
+        out = []
+        values = window.values()
+        for v in values:
+            out.append(str(v))
+        
+        out = opts.csv_sep.join(out)
+        out = out.rstrip(opts.csv_sep)
+        if opts.debug:
+            print(out + " ----- " + type)
+        else:
+            print(out)
+    else:
+        print("%s.%02u: %s" % (
+            time.strftime("%Y-%m-%d %H:%M:%S",
+                          time.localtime(m._timestamp)),
+                          int(m._timestamp*100.0)%100, m))
+        
---
+++ pymavlink/tools/slugslogconvert.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+"""\
+slugslogconvert.py is a GUI front-end for mavlogdump.py that is set to convert
+mavlink binary log files to csv format for MATLAB.
+
+Notes:
+-----
+* 2013-9-20 -- dagoodman
+    Started tool.
+
+Copyright 2012 David Goodman (dagoodman@soe.ucsc.edu)
+Released under GNU GPL version 3 or later
+
+"""
+import os
+#import re
+import pprint
+from subprocess import call
+
+# Python 2.x and 3.x compatability
+try:
+    from tkinter import *
+    import tkinter.filedialog
+    import tkinter.messagebox
+except ImportError as ex:
+    # Must be using Python 2.x, import and rename
+    from Tkinter import *
+    import tkFileDialog
+    import tkMessageBox
+
+    tkinter.filedialog = tkFileDialog
+    del tkFileDialog
+    tkinter.messagebox = tkMessageBox
+    del tkMessageBox
+
+#sys.path.append(os.path.join('pymavlink','generator'))
+#from mavgen import *
+
+DEBUG = True
+title = "SLUGS Log Converter"
+conversion_script = "mavlogdump_samplehold.py"
+message_types = "ATTITUDE,LOCAL_POSITION_NED,GPS_RAW_INT,GPS_DATE_TIME,SLUGS_NAVIGATION,RAW_IMU,SCALED_PRESSURE,CPU_LOAD,HEARTBEAT,SYS_STATUS,RC_CHANNELS_RAW,SERVO_OUTPUT_RAW,SCALED_IMU,RAW_PRESSURE,MID_LVL_CMDS,SENSOR_DIAG,VOLT_SENSOR,STATUS_GPS,NOVATEL_DIAG,PTZ_STATUS"
+conversion_args = "--format=csv --no-timestamps --dialect=slugs --types={0} --csv_sep=\",tab\"".format(message_types)
+error_limit = 5
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack_propagate(0)
+        self.grid( sticky=N+S+E+W)
+        self.createWidgets()
+        self.pp = pprint.PrettyPrinter(indent=4)
+
+    """\
+    Creates the gui and all of its content.
+    """
+    def createWidgets(self):
+
+
+        #----------------------------------------
+        # Create the binary log file entry
+
+        self.binary_log_value = StringVar()
+        self.binary_log_label = Label( self, text="Binary log" )
+        self.binary_log_label.grid(row=0, column = 0)
+        self.binary_log_entry = Entry( self, width = 26, textvariable=self.binary_log_value )
+        self.binary_log_entry.grid(row=0, column = 1)
+        self.binary_log_button = Button (self, text="Browse", command=self.browseBinaryLogFile)
+        self.binary_log_button.grid(row=0, column = 2)
+
+        #----------------------------------------
+        # Create the Out entry
+
+        self.out_value = StringVar()
+        self.out_label = Label( self, text="Output CSV" )
+        self.out_label.grid(row=1,column = 0)
+        self.out_entry = Entry( self, width = 26, textvariable=self.out_value )
+        self.out_entry.grid(row=1, column = 1)
+        self.out_button = Button (self, text="Browse", command=self.browseOutputFile)
+        self.out_button.grid(row=1, column = 2)
+
+        #----------------------------------------
+        # Create the generate button
+
+        self.convert_button = Button ( self, text="Convert", command=self.convertLog)
+        self.convert_button.grid(row=4,column=1)
+
+    """\
+    Open a file selection window to choose the binary mavlink log file.
+    """
+    def browseBinaryLogFile(self):
+        binary_log_file = tkinter.filedialog.askopenfilename(parent=self, title='Choose a binary .mavlink log file')
+        if DEBUG:
+            print("Binary log: " + binary_log_file)
+        if binary_log_file != None:
+            self.binary_log_value.set(binary_log_file)
+
+    """\
+    Open a file selection window to choose an output csv file.
+    """
+    def browseOutputFile(self):
+        mavlinkFolder = os.path.dirname(os.path.realpath(__file__))
+        output_file = tkinter.filedialog.askopenfilename(parent=self,title='Please choose an output .csv file name')
+        if DEBUG:
+            print("Output: " + output_file)
+        if output_file != None:
+            self.out_value.set(output_file)
+
+    """\
+    Converts the binary mavlink log file to a plaintext csv file.
+    """
+    def convertLog(self):
+        # Verify settings
+        if not self.binary_log_value.get():
+            tkinter.messagebox.showerror('Error Converting Log File','A binary .mavlink log file must be specified.')
+            return
+
+        if not self.out_value.get():
+            tkinter.messagebox.showerror('Error Converting Log File', 'An output .csv file must be specified.')
+            return
+
+
+        if os.path.exists(self.out_value.get()):
+            if not tkinter.messagebox.askokcancel('Overwrite output log file?','The output file \'{0}\' already exists. The file will be overwritten.'.format(self.out_value.get())):
+                return
+
+        # Run log conversion script
+        args = conversion_args + " " + self.binary_log_value.get() + " > " + self.out_value.get()
+        cmd = "python " + conversion_script + " " + args
+        result = 0
+
+        if DEBUG:
+            print("Converting log file")
+            self.pp.pprint(cmd)
+        try:
+            result = call(cmd,shell=True)
+            if result == 0:
+                tkinter.messagebox.showinfo('Successfully Converted Log File', 'Log file was converted succesfully.')
+            else:
+                raise Exception(conversion_script + " returned with error code " + str(result) + ". See terminal for details.")
+
+        except Exception as ex:
+            #exStr = formatErrorMessage(str(ex));
+            exStr  = str(ex)
+            if DEBUG:
+                print('An occurred while converting log file: \n\t{0!s}'.format(ex))
+            tkinter.messagebox.showerror('Error Converting Log File','{0!s}'.format(exStr))
+            return
+
+"""\
+Format the mavgen exceptions by removing "ERROR: ".
+"""
+def formatErrorMessage(message):
+    #reObj = re.compile(r'^(ERROR):\s+',re.M);
+    #matches = re.findall(reObj, message);
+    #message = re.sub(reObj, '\n', message);
+
+    #prefix = ("An error occurred in mavlogdump:" if len(matches) == 1 else "Errors occured in mavgen:\n")
+
+    #return prefix + message
+    return message
+
+
+# End of Application class
+# ---------------------------------
+
+# ---------------------------------
+# Start
+
+if __name__ == '__main__':
+  app = Application()
+  app.master.title(title)
+  app.mainloop()
